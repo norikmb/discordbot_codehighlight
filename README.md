00a012092f570d5164538d4eefb33d51f6dc3380
kambe <nori.k3636@gmail.com>
2021-11-29 23:06:21 +0900

    pin 機能の追加

---
+++ README.md
@@ -0,0 +1,25 @@
+# AwardsBot
+
+## 開発環境
+```
+node -v 
+v16.13.0
+
+npm -v
+7.24.0
+```
+
+## 環境構築
+```
+npm install discord.js
+npm install dotenv
+npm install typescript --save-dev
+npm install --save-dev eslint
+npx tsc --init
+```
+
+## 実行
+```
+npx tsc src/index.ts
+node src/index.js
+```
---
+++ src/index.ts
@@ -0,0 +1,97 @@
+import { Client, Intents } from "discord.js";
+import fetch from "node-fetch";
+import { config } from "dotenv";
+
+config();
+
+const client = new Client({
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+  ],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"],
+});
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+const atodemiru = "808566487983587338";
+client.on("messageReactionAdd", async (reaction, user) => {
+  console.log("test");
+  if (reaction.message.partial) {
+    try {
+      await reaction.message.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message: ", error);
+    }
+  }
+  if (reaction.message.channel.id === atodemiru && reaction.count === 1) {
+    reaction.message.pin();
+  }
+  // console.log(reaction.count);
+  // console.log(`${user.username} reacted with "${reaction.emoji.name}".`);
+});
+
+client.on("messageReactionRemove", async (reaction, user) => {
+  if (reaction.message.partial) {
+    try {
+      await reaction.message.fetch();
+    } catch (error) {
+      console.error("Something went wrong when fetching the message: ", error);
+    }
+  }
+
+  console.log(
+    `${user.username} removed their "${reaction.emoji.name}" reaction.`
+  );
+});
+client.on("messageCreate", async (message) => {
+  if (message.author.bot) {
+    return;
+  }
+  if (message.content.startsWith("https://github.com/")) {
+    const url = message.content;
+    const newurl = url.replace(
+      /^(.*\/\/github.com\/.+\/.+\/)blob(\/.+)$/i,
+      "$1raw$2"
+    );
+    const paragraph = url.split("#");
+    if (paragraph[1] != null) {
+      console.log("Where you want to display is existing.");
+      (async () => {
+        try {
+          const response = await fetch(newurl);
+          let sendtext = "";
+          const body = await response.text();
+          const lines = body.split("\n");
+          // 拡張子を取得
+          const fileType = paragraph[0]
+            .split("/")
+            .slice(-1)[0]
+            .split(".")
+            .slice(-1)[0];
+          const LineNumber = paragraph[1].split("-");
+          // 行始めと行終わりを取得
+          if (LineNumber.length === 2) {
+            const begin = Number(LineNumber[0].substring(1)) - 1;
+            const end = Number(LineNumber[1].substring(1));
+            console.log({ begin, end });
+            sendtext = lines.slice(begin, end).join("\n");
+          } else if (LineNumber.length === 1) {
+            const begin = Number(LineNumber[0].substring(1)) - 1;
+            sendtext = lines.slice(begin, begin + 1).join("\n");
+          } else {
+            console.log("error");
+          }
+
+          // テンプレートリテラル
+          message.channel.send(` \`\`\`${fileType}\n${sendtext}  \`\`\``);
+        } catch (error) {
+          console.log(error);
+        }
+      })();
+    }
+  }
+});
+client.login(process.env.DISCORD_TOKEN);
